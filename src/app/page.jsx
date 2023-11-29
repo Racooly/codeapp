4653ac8ee922ebbf05a58d37b3959fa467a20c67
Racooly <sandroiremadze14@gmail.com>
2023-11-29 19:34:50 +0400

    routing

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,3 @@
-import Navbar from '@/components/Navbar/Navbar'
 import styles from './page.module.css'
 
 export default function Home() {
---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Portfolio() {
+  return (
+    <div> This is Portfolio</div>
+  )
+}
+export default Portfolio  
