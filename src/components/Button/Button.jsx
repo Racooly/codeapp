f7f7f87a1469845be35bc350c4edd157ebb25f7e
Racooly <sandroiremadze14@gmail.com>
2023-12-13 20:18:40 +0400

    categories

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from './button.module.css'
 import Link from 'next/link'
 
-function Button({ title, url}) {
+function Button({ title }) {
   return (
-    <Link href={url}>
+    <Link href={'/'}>
         <button className={styles.button}>{title}</button>
     </Link>
   )
